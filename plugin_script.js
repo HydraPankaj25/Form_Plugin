538eb9fa0f0a5918d0c80b256962fc5a22c62e9e
HydraPankaj25 <pm2582001@gmail.com>
2022-04-27 21:09:11 +0530

    Form Plugin that can be used by any website

---
+++ plugin_script.js
@@ -0,0 +1,8 @@
+function activate(){
+    console.log("function called");
+    document.getElementsByClassName("container")[0].style.display="block";
+}
+function remove(){
+    console.log("function called");
+    document.getElementsByClassName("container")[0].style.display="none";
+}
